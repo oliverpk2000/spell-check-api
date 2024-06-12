8391484def53e327c24fd4c51806705ef0231203
opk <oliver.pecek@gmail.com>
2024-06-12 22:46:31 +0200

    implemented routing, no actual spellchecking yet

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use axum::{routing::get, Router};
+use axum::extract::Path;
+use axum::{routing::get, Json, Router};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(|| async { "the api is at /words/:word" }));
+        .route("/", get(|| async { "the api is at /words/:word" }))
+        .route("/words/:word", get(get_corrected_words));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_corrected_words(Path(arg): Path<String>) -> Json<Vec<String>> {
+    let correct_words = vec![arg, "test".parse().unwrap(), "test2".parse().unwrap()];
+    Json(correct_words)
 }
